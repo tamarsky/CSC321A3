11e280b8b7fc41a08dbc668cbc68aaac98621fbc
Chloe <chloe.duan@hotmail.com>
2016-03-20 18:55:49 -0500

    made partition_data_set, crop_faces universal to parts 1 and 2

--- crop_faces.py
+++ crop_faces.py
@@ -13,12 +13,25 @@ from scipy.ndimage import filters
 import urllib
 
 
-def setup():
-    if not os.path.exists('cropped'):
-        os.mkdir('cropped') 
+def setup(dirname):
+    '''
+    if part 1: dirname = 'cropped'
+    if part 2: dirname = 'cropped_rgb'
+    '''
+    if not os.path.exists(dirname): #'cropped' or 'cropped_rgb'
+        os.mkdir(dirname) 
         
-def crop_all(colour=False):
-    setup()
+def crop_all(part):
+    if part ==1 :
+        target_dirname = '1cropped/'
+        gray = 1
+        dim = (32, 32)
+    elif part ==2:
+        target_dirname = '2cropped_rgb/'
+        gray = 0
+        dim = (227, 227, 3)
+    setup(target_dirname)
+    
     for filename in os.listdir('uncropped'):
         hash_from_file = sha256(file.read(open('uncropped/' + filename))).hexdigest()
         if filename.split('.')[2] == hash_from_file:   
@@ -32,14 +45,20 @@ def crop_all(colour=False):
             new_name = filename.split('.')[0] + '.' + filename.split('.')[3]
         
             try:
-                im = imread('uncropped/'+filename, 1)[y1:y2, x1:x2]
-                if not colour:
-                    gray()
-                resized_im = imresize(im, (32, 32))
-                imsave('cropped/' + new_name, resized_im)
+                im = imread('uncropped/'+filename, gray)[y1:y2, x1:x2]
+                resized_im = imresize(im, dim) # dim = (32, 32) or (227, 227, 3) for p1, p2 respectfully
+                imsave(target_dirname + new_name, resized_im) # target_dirname = 'cropped' / 'cropped_rgb'
             except IOError:
                 print 'IOError: Skipped ' + new_name
                 pass
             except ValueError:
                 print 'ValueError: Skipped ' + new_name
                 pass
+
+if __name__ == "__main__":
+    '''
+    get_data needs to be called before this
+    i.e. uncropped folder must be populated
+    '''
+    crop_all(1)
+    crop_all(2)
--- partition_data_set.py
+++ partition_data_set.py
@@ -3,17 +3,24 @@ import os.path
 from scipy.misc import imread
 
 
-def setup():
-    if not os.path.exists('training_set'):
-        os.mkdir('training_set') 
-    if not os.path.exists('validation_set'):
-        os.mkdir('validation_set')
-    if not os.path.exists('test_set'):
-        os.mkdir('test_set')        
+def setup(part):
+    if not os.path.exists(str(part) + 'training_set'):
+        os.mkdir(str(part) + 'training_set') 
+    if not os.path.exists(str(part) + 'validation_set'):
+        os.mkdir(str(part) + 'validation_set')
+    if not os.path.exists(str(part) + 'test_set'):
+        os.mkdir(str(part) + 'test_set')        
                 
-def partition_data():
+def partition_data(part):
     #partition data into Training set, Validation set, Test set
-    setup()
+    setup(part)
+    
+    if part == 1:
+        dirname = '1cropped/'
+    elif part == 2:
+        dirname = '2cropped_rgb/'
+    if not os.path.exists(dirname):
+        os.mkdir(dirname) 
     
     act = ['Gerard Butler', 'Daniel Radcliffe','Michael Vartan', 'Lorraine Bracco',
     'Peri Gilpin', 'Angie Harmon']
@@ -22,44 +29,58 @@ def partition_data():
     counts = [0, 0, 0, 0, 0, 0]
     
     act_count = dict(zip(act_lower, counts))
-    for filename in os.listdir('cropped_rgb'):
+    for filename in os.listdir(dirname):
         if filename[:6] in act_lower:
             act_count[filename[:6]] += 1
             if act_count[filename[:6]] <= 70:
-                copyfile('cropped/'+filename, 'training_set/'+filename)
-                print filename+' in '+'training_set'
+                copyfile(dirname+filename, str(part) + 'training_set/'+filename)
+                print filename+' in '+ str(part)+ 'training_set'
             elif act_count[filename[:6]]%2 == 0:
-                copyfile('cropped/'+filename, 'validation_set/'+filename)
-                print filename+' in '+'validation_set'                
+                copyfile(dirname+filename, str(part) + 'validation_set/'+filename)
+                print filename+' in '+ str(part) + 'validation_set'                
             else:
-                copyfile('cropped/'+filename, 'test_set/'+filename)
-                print filename+' in '+'test_set'
+                copyfile(dirname+filename, str(part) + 'test_set/'+filename)
+                print filename+' in '+ str(part) + 'test_set'
                 
                 
-def get_train_dict():
+def get_train_dict(part):
     '''Return dictionary of training set where keys are labels (actors' names as 
     lower case strings e.g. 'bracco') and values are lists of numpy arrays of 
     images of that actor's face
     '''
+    if part ==1:
+        gray = 1 # grayscale
+    elif part == 2:
+        gray = 0 # not grayscale
+        
     train_dict = {}
-    for file in os.listdir('training_set'):
+    for file in os.listdir(str(part)+'training_set'):
         label = file.split('.')[0].rstrip('1234567890')
-        im = imread('training_set/' + file, 1).flatten()
+        im = imread(str(part)+'training_set/' + file, gray)
+        if part == 1:
+            im = im.flatten()
         if not train_dict.has_key(label):
             train_dict[label] = [im]
         else:
             train_dict[label].append(im)
     return train_dict
     
-def get_test_dict():
+def get_test_dict(part):
     '''Return dictionary of test set where keys are labels (actors' names as 
     lower case strings e.g. 'bracco') and values are lists of numpy arrays of 
     images of that actor's face
     '''
+    if part ==1:
+        gray = 1 # grayscale
+    elif part == 2:
+        gray = 0 # not grayscale
+        
     test_dict = {}
-    for file in os.listdir('test_set'):
+    for file in os.listdir(str(part)+'test_set'):
         label = file.split('.')[0].rstrip('1234567890')
-        im = imread('test_set/' + file, 1).flatten()
+        im = imread(str(part)+'test_set/' + file, gray)
+        if part == 1:
+            im = im.flatten()
         if not test_dict.has_key(label):
             test_dict[label] = [im]
         else:
@@ -71,6 +92,15 @@ def get_dict_size(dic):
     for key in dic:
         dict_size += len(dic[key])
     return dict_size
+    
+if __name__ == "__main__":
+    '''
+    crop_all(#) need to called respectfully 
+    i.e. cropped folders need to be made already before calling this
+    '''
+    partition_data(1)
+    partition_data(2)
+
     
     
     
